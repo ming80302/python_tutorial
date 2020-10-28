e3acbc6314ebfef32011e758e65ab9989d1df8f8
Ming Ge <ming80302@gmail.com>
2020-10-28 16:43:21 -0600

    parsing file

--- mysci.py
+++ mysci.py
@@ -1,27 +1,34 @@
-print("Hello, world")
+# Initialized my data variable
+data = []
 
 # read the data file
 filename ="data/wxobs20170821.txt"
 
-datafile = open(filename, "r")
+# datafile = open(filename, "r")
 # read a line
-#print(datafile.readline())
-#print(datafile.readline())
-#print(datafile.readline())
+# print(datafile.readline())
+# print(datafile.readline())
+# print(datafile.readline())
 
 # the other way to read a data file
-data = datafile.read()
-
-# DEGUG
-#print(data)
+# data = datafile.read()
 
 # important to close for memory issues
-datafile.close()
+# datafile.close()
 
 
 # for a cleaner version using with as a context manager, automaticly closed after reading it
 with open(filename, 'r') as datafile:
-    data = datafile.read()
+    #data = datafile.read()
+    # read the first three lines
+    for _ in range(3):
+        datafile.readline()
 
-print(data)
+    # read and parse the rest of the file
+    for line in datafile:
+        datum = line.split()	# default split according to space, '/t' is for tab
+        data.append(datum)
+
+
+
 
