ef7c7be1680e6270dd71499fb2bee6b6291d8720
Ming Ge <ming80302@gmail.com>
2020-12-01 13:51:35 -0700

    Reading windspeed in too

--- mysci.py
+++ mysci.py
@@ -1,5 +1,14 @@
+# Column names and column indices to read
+columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7}
+
+# data types for each column 
+types = {'tempout':float, 'windspeed':float}
+
 # Initialized my data variable
-data = {'date':[], 'time':[], 'tempout':[]}
+#data = {'date':[], 'time':[], 'tempout':[]}
+data = {}
+for column in columns:
+    data[column] = []
 
 # read the data file
 filename ="data/wxobs20170821.txt"
@@ -16,11 +25,10 @@ filename ="data/wxobs20170821.txt"
 # important to close for memory issues
 # datafile.close()
 
-
 # for a cleaner version using with as a context manager, automaticly closed after reading it
 with open(filename, 'r') as datafile:
     #data = datafile.read()
-    # read the first three lines
+    # read the first three lines, and discard
     for _ in range(3):
         datafile.readline()
 
@@ -29,10 +37,13 @@ with open(filename, 'r') as datafile:
         #datum = line.split()	# default split according to space, '/t' is for tab
         #data.append(datum)
         split_line = line.split()
-        data['date'].append(split_line[0])
-        data['time'].append(split_line[1])
-        data['tempout'].append(float(split_line[2]))
-
-
-
-
+        #data['date'].append(split_line[0])
+        #data['time'].append(split_line[1])
+        #data['tempout'].append(float(split_line[2]))
+        for column in columns:
+            i = columns[column]
+            t = types.get(column, str)
+            value = t(split_line[i])
+            data[column].append(value)
+            
+print(data['tempout'])
